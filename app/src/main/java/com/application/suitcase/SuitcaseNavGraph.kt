30dbb48646e7bcded4e2b050c76d9c040dab7ace
Samuel Ajibade <samuelajibade22@gmail.com>
2024-01-19 18:06:13 +0100

    added compose navigation

---
+++ app/src/main/java/com/application/suitcase/SuitcaseNavGraph.kt
@@ -0,0 +1,71 @@
+package com.application.suitcase
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.application.suitcase.ui.auth.login.LoginScreen
+import com.application.suitcase.ui.auth.login.LoginViewModel
+import com.application.suitcase.ui.auth.passwordReset.PasswordResetScreen
+import com.application.suitcase.ui.auth.passwordReset.PasswordResetViewModel
+import com.application.suitcase.ui.auth.register.RegistrationScreen
+import com.application.suitcase.ui.auth.register.RegistrationViewModel
+import com.application.suitcase.ui.home.HomeScreen
+import com.application.suitcase.ui.home.HomeViewModel
+import com.application.suitcase.ui.onboarding.OnboardingScreen
+import com.application.suitcase.ui.suitcase.AddSuitcase
+import com.application.suitcase.ui.suitcase.SuitcaseViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+/**
+ * Destinations used in the ([SuitcaseApp])
+ */
+
+
+
+@Composable
+fun NavGraph(
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = NavigationRoutes.ONBOARDING_SCREEN) {
+
+
+    val loginViewModel = remember { LoginViewModel() }
+
+    val auth: FirebaseAuth = Firebase.auth
+
+    LaunchedEffect(auth) {
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            navController.navigate(NavigationRoutes.HOME_SCREEN)
+        } else {
+            navController.navigate(NavigationRoutes.ONBOARDING_SCREEN)
+        }
+    }
+
+
+    NavHost(navController = navController, startDestination = startDestination) {
+        composable(NavigationRoutes.ONBOARDING_SCREEN) {
+            OnboardingScreen(navController = navController)
+        }
+        composable(NavigationRoutes.LOGIN_SCREEN) {
+            LoginScreen(viewModel = loginViewModel, navController = navController)
+        }
+        composable(NavigationRoutes.REGISTRATION_SCREEN) {
+            RegistrationScreen(viewModel = RegistrationViewModel(), navController = navController)
+        }
+        composable(NavigationRoutes.PASSWORD_RESET_SCREEN) {
+            PasswordResetScreen(viewModel = PasswordResetViewModel(), navController = navController)
+        }
+        composable(NavigationRoutes.HOME_SCREEN) {
+            HomeScreen(viewModel = HomeViewModel(), navController = navController)
+        }
+        composable(NavigationRoutes.SUITCASE_SCREEN) {
+            AddSuitcase(viewModel = SuitcaseViewModel(), navController = navController)
+        }
+    }
+}
---
+++ app/src/main/java/com/application/suitcase/SuitcaseRoutes.kt
@@ -0,0 +1,14 @@
+package com.application.suitcase
+
+/**
+ * Screen Routes used in the ([SuitcaseApp])
+ */
+
+object NavigationRoutes {
+    const val ONBOARDING_SCREEN = "onboarding"
+    const val LOGIN_SCREEN = "login"
+    const val REGISTRATION_SCREEN = "registration"
+    const val PASSWORD_RESET_SCREEN = "password_reset"
+    const val HOME_SCREEN = "home"
+    const val SUITCASE_SCREEN = "add_suitcase"
+}
